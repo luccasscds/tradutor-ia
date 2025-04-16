1067f5a9d9ae383d19119c38dcb20185da8c9872
Lucas da silva costa <lucas@unicorp.com.br>
2025-04-16 13:58:29 -0300

    Remove verificação de internet desnecessária

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,10 +1,3 @@
 export const tools = {
-    async checkInternet() {
-        try {
-            const resp = await fetch('https://www.google.com', { method: 'HEAD',  });
-            if (!resp.ok) throw 'Sem conexão com a internet.';
-        } catch (error) {
-            throw 'Sem conexão com a internet.';
-        }
-    }
+    
 }
